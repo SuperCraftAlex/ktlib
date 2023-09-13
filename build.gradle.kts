2ddcb142fbb138ca33c526e9f07a1e2e6aa3aaf9
alex_s168 <63254202+SuperCraftAlex@users.noreply.github.com>
2023-09-13 21:05:23 +0200

    gradle?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    `maven-publish`
 }
 
 group = "me.alex_s168"
@@ -24,4 +25,16 @@ kotlin {
 
 application {
     mainClass.set("MainKt")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "me.alex_s168"
+            artifactId = "ktlib"
+            version = "1.0.0"
+
+            from(components["java"])
+        }
+    }
 }
